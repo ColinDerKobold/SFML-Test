8fe6982977ea8e4fcae0022cf743b6273a5de87d
Colin Wenzel <cowenzel@adp-merkur.com>
2026-08-24 12:06:07 +0200

    Vertex and Shapes + RGB

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <cmath>
 
-enum directions {stand, look, nohat, nohatlook};
+sf::Color HSVToRGB(float h, float s, float v) {
+
+    float hPrime = h / 60.0f;
+    unsigned int hIndex = unsigned int(hPrime) % 6;
+    float chroma = s * v;
+    float min = (v-chroma);
+    float x = chroma * (1.0f - abs(fmod(hPrime, 2.0f) - 1.0f));
+
+    float outRGB[6][3] = {
+        { chroma, x, 0.0f },
+        { x, chroma, 0.0f },
+        {0.0f, chroma, x },
+        {x, 0.0f, chroma },
+        {chroma, 0.0f, x },
+        {chroma,x,0.0f}
+    };
+
+    float rF = (outRGB[hIndex][0] + min);
+    float gF = (outRGB[hIndex][1] + min);
+    float bF = (outRGB[hIndex][2] + min);
+    rF *= 255.0f;
+    gF *= 255.0f;
+    bF *= 255.0f;
+
+    std::uint8_t rI = std::uint8_t(rF);
+    std::uint8_t gI = std::uint8_t(gF);
+    std::uint8_t bI = std::uint8_t(bF);
+
+    return sf::Color(rI,gI,bI);
+}
 
 int main() {
     unsigned int width = 640;
@@ -11,23 +41,74 @@ int main() {
         sf::Style::Close | sf::Style::Resize | sf::Style::Titlebar );
     window->setFramerateLimit(60);
 
-    sf::Texture texture;
+    int n = 24;
 
-    if (!texture.loadFromFile("Sprites/download sprite.png")) {
-        std::cerr << "ERROR::COULD NOT LOAD FILE::Sprites/download sprite.png" << std::endl;
-        return -1;
+    sf::VertexArray lines;
+    lines.setPrimitiveType(sf::PrimitiveType::Lines);
+    lines.resize(n);
+
+    sf::VertexArray lineStrip;
+    lineStrip.setPrimitiveType(sf::PrimitiveType::LineStrip);
+    lineStrip.resize(n);
+
+    sf::VertexArray triangles;
+    triangles.setPrimitiveType(sf::PrimitiveType::Triangles);
+    triangles.resize(width * height * 6);
+
+    float size = 48.0f;
+
+    //sf::Vector2f v0 = {0.0f, 0.0f};
+    //sf::Vector2f v1 = {size, 0.0f};
+    //sf::Vector2f v2 = {0.0f, size};
+    //sf::Vector2f v3 = {size, size};
+
+    //triangles[0].position = v0;
+    //triangles[1].position = v1;
+    //triangles[2].position = v2;
+    //triangles[3].position = v3;
+    //triangles[4].position = v2;
+    //triangles[5].position = v1;
+
+    for (int i = 0; i < width; ++i) {
+        for (int j = 0; j < height; ++j) {
+            //triangles[i].position += {width / 1.5f, height / 1.5f};
+            const int index = 6 * (i * height + j);
+            sf::Vector2f v0 = {size * i, size * j};
+            sf::Vector2f v1 = {size * i, size * (j+1)};
+            sf::Vector2f v2 = {size * (i+1), size * j};
+            sf::Vector2f v3 = {size * (i+1), size * (j+1)};
+
+            triangles[index + 0].position = v0;
+            triangles[index + 1].position = v1;
+            triangles[index + 2].position = v2;
+
+            triangles[index + 3].position = v3;
+            triangles[index + 4].position = v2;
+            triangles[index + 5].position = v1;
+
+            triangles[index + 0].color = HSVToRGB(0.0f, 1.0f, 1.0f);
+            triangles[index + 1].color = HSVToRGB(60.0f, 1.0f, 1.0f);
+            triangles[index + 2].color = HSVToRGB(120.0f, 1.0f, 1.0f);
+            triangles[index + 3].color = HSVToRGB(180.0f, 1.0f, 1.0f);
+            triangles[index + 4].color = HSVToRGB(240.0f, 1.0f, 1.0f);
+            triangles[index + 5].color = HSVToRGB(300.0f, 1.0f, 1.0f);
+
+            //triangles[i].color = HSVToRGB(60.0f * i, 1.0f, 1.0f);
+        }
     }
-    sf::Sprite sprite(texture);
 
-    sf::IntRect dir[4];
+    for (int i = 0; i < n; i++) {
+        sf::Vector2f pos = {float(i*cos(i)), float(i*sin(i))};
+        float hue = i * (360.0f/n);
 
-    for (int i = 0; i < 4; i++) {
-        dir[i] = sf::IntRect({{115+183*i, 233}, {150,180}});
+        lines[i].position = pos;
+        lines[i].position += {width / 4.0f, height / 4.0f};
+        lines[i].color = HSVToRGB(hue, 1.0f, 1.0f);
+
+        lineStrip[i].position = pos;
+        lineStrip[i].position += {width / 2.0f, height / 2.0f};
+        lineStrip[i].color = HSVToRGB(hue, 1.0f, 1.0f);
     }
-    sprite.setTextureRect(dir[stand]);
-    sprite.setOrigin({75,90});
-    sprite.setPosition({width/2.0f,height/2.0f});
-    sprite.setColor(sf::Color(0x6495EDFF));
 
     while (window->isOpen()) {
         while (std::optional event = window->pollEvent()) {
@@ -40,27 +121,13 @@ int main() {
             }
         }
 
-        sprite.rotate(sf::degrees(1));
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::S)) {
-            sprite.move({0.0f, 1.0f});
-            sprite.setTextureRect(dir[stand]);
-        }else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::W)) {
-            sprite.move({0.0f, -1.0f});
-            sprite.setTextureRect(dir[nohatlook]);
-        }else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::A)) {
-            sprite.move({-1.0f, 0.0f});
-            sprite.setTextureRect(dir[nohat]);
-        }else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::D)) {
-            sprite.move({1.0f, 0.0f});
-            sprite.setTextureRect(dir[look]);
-        }
-
         //Render
         window -> clear();
 
         //Drawing
-        window->draw(sprite);
+        window->draw(lines);
+        window->draw(lineStrip);
+        window->draw(triangles);
 
         window->display();
     }
